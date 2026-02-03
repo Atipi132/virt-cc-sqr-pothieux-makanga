54fde254a34828a45371c2692452c1e22f71ec69
Hugo Pothieux <hugo.pothieux@protonmail.com>
2026-02-03 19:39:38 +0100

    Modified backend app.py

--- application/backend/app.py
+++ application/backend/app.py
@@ -56,11 +56,11 @@ def calculate():
             'calculation': calculation
         }
 
-        # Send to RabbitMQ
-        send_to_queue(message)
-
         # Initialize status in Redis (optional, but good practice)
         r.set(task_id, 'PENDING')
+
+        # Send to RabbitMQ
+        send_to_queue(message)
 
         return jsonify({'task_id': task_id}), 202
 
